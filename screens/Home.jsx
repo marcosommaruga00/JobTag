0bb38ca99317d02b204a02e2a5a98513507394be
marcosomma00 <marcosommaruga.00@gmail.com>
2023-06-17 18:49:34 +0200

    home logo + notification icon

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,4 +1,6 @@
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet, View, Image } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Entypo } from '@expo/vector-icons';
 import { BaseText } from '../components/Text';
 
 export default function Detail () {
@@ -6,7 +8,10 @@ export default function Detail () {
         <View>
             <ImageBackground source={require('../assets/bg.png')} style={{width: '100%', height: '100%'}}>
                 <View style={styles.container}>
-                    <BaseText align="left">La miglior app di sempre</BaseText>
+                    <View style={styles.logo} >
+                        <Image source={require('../assets/logoSmall.png')} style={{width: 138, height: 24, marginTop: 50}}/>
+                        <Entypo name="notification" size={24} color="black" style={{marginTop: 50}}/>
+                    </View>
                 </View>
             </ImageBackground>
         </View>
@@ -18,5 +23,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    },
+    logo: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'left',
+        justifyContent: 'space-between',
+        width: 320,
     },
 });
