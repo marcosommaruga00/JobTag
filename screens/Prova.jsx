1d3e88caf08af9e7964d87666c9c8520d1c31d09
marcosomma00 <marcosommaruga.00@gmail.com>
2023-06-19 18:01:24 +0200

    img persone

--- screens/Prova.jsx
+++ screens/Prova.jsx
@@ -1,9 +1,156 @@
-import { ImageBackground, StyleSheet, View, Image } from "react-native";
+import { ImageBackground, StyleSheet, View, Image, SafeAreaView, ScrollView, Dimensions } from "react-native";
+import React, { Component } from "react"
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { BaseText } from '../components/Text';
+import { CallButton, SettingButton, QRButton } from "../components/Button";
+import { useNavigation } from '@react-navigation/native';
 
-export default function Prova () {
+const ITEM_WIDTH = Dimensions.get("window").width
+const ITEM_HEIGHT = 200
+
+const cards = [
+    { title: "Persona1", posterUrl: require("../assets/persone/persona1") },
+    { title: "Persona2", posterUrl: require("../assets/persone/persona2") },
+    { title: "Persona3", posterUrl: require("../assets/persone/persona3") },
+    { title: "Persona4", posterUrl: require("../assets/persone/persona4") },
+    { title: "Persona5", posterUrl: require("../assets/persone/persona5") },
+]
+
+export default function Home () {
+
+    const navigation = useNavigation();
+    const goToThirdPage = () => {
+        navigation.navigate('Prova');
+    };
+    const goToHome = () => {
+        navigation.navigate('Home');
+    };
+    const goToNotification = () => {
+        navigation.navigate('Notification');
+    };
+    const goToQrcode = () => {
+        navigation.navigate('Qrcode');
+    };
+    const goToSettings = () => {
+        navigation.navigate('Settings');
+    };
+
     return (
         <View>
-            <ImageBackground source={require('../assets/bg.png')} style={{width: '100%', height: '100%'}} />
+            <ImageBackground source={require('../assets/bg.png')} style={{width: '100%', height: '100%'}}>
+                <View style={styles.container}>
+                    <View style={styles.logo} >
+                        <Image source={require('../assets/logoSmall.png')} style={{width: 138, height: 24, marginTop: 50}}/>
+                        <Entypo.Button name="notification" size={24} backgroundColor={'transparent'} color="#142A39" style={{marginTop: 40}} onPress={goToNotification}/>
+                    </View>
+                    <View style={styles.center}>
+                        {/* carosello */}
+                        <View style={{flex: 3, borderStyle: 'solid', borderWidth: 1, borderColor: 'yellow', width: 365, height: 218}}>
+                            <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+                                <ScrollView
+                                    horizontal={true}
+                                    decelerationRate={"normal"}
+                                    snapToInterval={ITEM_WIDTH}
+                                    bounces={false}
+                                    style={{ marginTop: 40, paddingHorizontal: 0 }}
+                                    showsHorizontalScrollIndicator={false}
+                                    scrollEventThrottle={12}
+                                >
+                                    {cards.map((item, idx) => {
+                                    return (
+                                        <View
+                                        style={{
+                                            width: ITEM_WIDTH,
+                                            height: ITEM_HEIGHT,
+                                        }}
+                                        >
+                                        <ImageBackground
+                                            source={item.posterUrl}
+                                            style={{
+                                            flex: 1,
+                                            resizeMode: "cover",
+                                            justifyContent: "center",
+                                            }}
+                                        />
+                                        </View>
+                                    )
+                                    })}
+                                </ScrollView>
+                            </SafeAreaView>
+                        </View>
+                        <View style={{flex: 3, borderStyle: 'solid', borderWidth: 1, borderColor: 'blue', width: 365, height: 218}}>
+                        <BaseText>Carosello luoghi</BaseText>
+                        </View>
+                    </View>
+                    <View style={styles.bottom}>
+
+                        {/* View icon settings */}
+                        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: 320, backgroundColor: 'rgba(255, 255, 255, 0)', zIndex: 2}}>
+                            <SettingButton onPress={() => navigation.navigate("Prova")}>
+                                <Ionicons.Button name="settings" size={42} backgroundColor={'rgba(255, 255, 255, 0)'} borderRadius={99} color="white" iconStyle={{paddingLeft: 2}} onPress={goToSettings}/>
+                            </SettingButton>
+                        </View>
+
+                        {/* View icon QRCode */}
+                        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: 320, backgroundColor: 'rgba(255, 255, 255, 0)', zIndex: 2}}>
+                            <QRButton onPress={() => navigation.navigate("Prova")}>
+                                <MaterialIcons name="qr-code-scanner" size={42} backgroundColor={'rgba(255, 255, 255, 0)'} borderRadius={99} color="white" iconStyle={{paddingLeft: 2}} onPress={goToQrcode}/>
+                            </QRButton>
+                        </View>
+
+                        {/* View icon call */}
+                        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: 320, backgroundColor: 'rgba(255, 255, 255, 0)', zIndex: 2}}>
+                            <CallButton onPress={() => navigation.navigate("Home")} >
+                                <Ionicons.Button name="call" size={42} backgroundColor={'#30D158'} borderRadius={99} color="white" iconStyle={{paddingLeft: 2}} onPress={goToHome}/>
+                            </CallButton>
+                        </View>
+                        {/* Imag menù */}
+                        <Image source={require('../assets/menu.png')} style={{width: '100%', height: 247}}/>
+                    </View>
+                </View>
+            </ImageBackground>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    },
+    logo: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'left',
+        justifyContent: 'space-between',
+        width: 320,
+        backgroundColor: 'rgba(255, 255, 255, 0)',
+    },
+    bottom:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        marginBottom: 0,
+        width: '100%',
+    },
+    center:{
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 0,
+        marginBottom: 150,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'red',
+        marginLeft: 25,
+        width: 365,
+        height: 436,
+    },
+});
+
+/* backgroundColor={'#30D158'} borderRadius={999} color="white" iconStyle={{paddingLeft: 2}}  */
+/* style={{ zIndex: 3, marginBottom: -98}} */
