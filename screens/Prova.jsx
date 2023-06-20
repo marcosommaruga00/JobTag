218b7589c6a0a6d6b04bda643b9c951f11db3a16
marcosomma00 <marcosommaruga.00@gmail.com>
2023-06-20 17:37:57 +0200

    fix logo prova

--- screens/Prova.jsx
+++ screens/Prova.jsx
@@ -32,7 +32,7 @@ export default function Home () {
             <ImageBackground source={require('../assets/bg.png')} style={{width: '100%', height: '100%'}}>
                 <View style={styles.container}>
                     <View style={styles.logo} >
-                        <Image source={require('../assets/logoSmall.png')} style={{width: 138, height: 24, marginTop: 50}}/>
+                        <Image source={require('../assets/logoSmall5.png')} style={{width: 138, height: 24, marginTop: 50}}/>
                         <Entypo.Button name="notification" size={24} backgroundColor={'transparent'} color="#142A39" style={{marginTop: 40}} onPress={goToNotification}/>
                     </View>
                     <View style={styles.center}>
